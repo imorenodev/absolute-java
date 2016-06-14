4d789bfc9f7cbbdfafa112d770de633509d87180
imorenodev@gmail.com <imorenodev@gmail.com>
2016-06-14 10:26:58 -0400

    practice with flow control and classes

---
+++ chapter-four/DateFirstTryDemo.java
@@ -0,0 +1,18 @@
+public class DateFirstTryDemo {
+  public static void main(String[] args) {
+    DateFirstTry date1, date2;
+    date1 = new DateFirstTry();
+    date2 = new DateFirstTry();
+
+    date1.month = "April";
+    date1.day = 20;
+    date1.year = 1987;
+    date1.writeOutput();
+
+    date2.month = "May";
+    date2.day = 31;
+    date2.year = 1988;
+    date2.writeOutput();
+
+  }
+}
