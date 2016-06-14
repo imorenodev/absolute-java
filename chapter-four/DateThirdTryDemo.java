4c654f0cc285b0f337045b17cb63c1b606294375
imorenodev@gmail.com <imorenodev@gmail.com>
2016-06-14 11:49:41 -0400

    practice with passing arguments to methods that accept parameters

---
+++ chapter-four/DateThirdTryDemo.java
@@ -0,0 +1,8 @@
+public class DateThirdTryDemo {
+  public static void main(String[] args) {
+    DateThirdTry date1 = new DateThirdTry();
+    int year = 1987;
+    date1.setDate(6, 17, year);
+    date1.writeOutput();
+  }
+}
